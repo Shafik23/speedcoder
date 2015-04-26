789ddaf9b8edd6bc6cf9d9cc77804f02a7cd75f3
Shafik <shafik23@gmail.com>
2015-04-26 15:49:45 -0700

    Selection on keyboard input now works.

--- scripts/gui.js
+++ scripts/gui.js
@@ -1,1 +1,27 @@
+function loadCodeSnippet() {
+   var snippetBox = document.getElementById("codeSnippetArea");
 
+   // selectionStart and selectionEnd are mutable by the user, so
+   // we'll use our own property to keep track of where the
+   // user is.
+   snippetBox.cursorPos = 0;
+
+   // grab a raw-text code snippet
+   snippetBox.value = httpGet("/snippet");
+
+   snippetBox.onkeypress = function() {
+      snippetBox.cursorPos = snippetBox.cursorPos + 1;
+      snippetBox.setSelectionRange(0, snippetBox.cursorPos);
+
+      console.log("cursorPos is : " + snippetBox.cursorPos);
+   }
+}
+
+function httpGet(url) {
+    var xmlHttp = null;
+
+    xmlHttp = new XMLHttpRequest();
+    xmlHttp.open("GET", url, false);
+    xmlHttp.send(null);
+    return xmlHttp.responseText;
+}
