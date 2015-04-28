f0aa82c12e83c5cefe5d3092a6444705cd0d3340
Shafik <shafik23@gmail.com>
2015-04-27 23:01:36 -0700

    Fixed auto-scroll-to-the-bottom issue

--- scripts/gui.js
+++ scripts/gui.js
@@ -4,17 +4,20 @@ function loadCodeSnippet() {
    // selectionStart and selectionEnd are mutable by the user, so
    // we'll use our own property to keep track of where the
    // user is.
-   snippetBox.cursorPos = 0;
+   // __ is the prefix used for the property since we're technically not 
+   // allowed to extend DOM objects in an adhoc fashion (but everyone does it 
+   // anyways).
+   snippetBox.__cursorPos = 0;
 
    // grab a raw-text code snippet
    snippetBox.value = httpGet("/snippet");
+   snippetBox.focus();
+   snippetBox.scrollTop = 0;
 
    snippetBox.onkeypress = function() {
-      snippetBox.cursorPos = snippetBox.cursorPos + 1;
-      snippetBox.setSelectionRange(0, snippetBox.cursorPos);
-
-      console.log("cursorPos is : " + snippetBox.cursorPos);
-   }
+      snippetBox.__cursorPos = snippetBox.__cursorPos + 1;
+      snippetBox.setSelectionRange(0, snippetBox.__cursorPos);
+   };
 }
 
 function httpGet(url) {
